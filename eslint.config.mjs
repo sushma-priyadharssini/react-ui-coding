d5727dc652b0e4664e7a64f312e0680fe01184f7
Sushma Priyadharssini KV <kvsushmapriyadharssini@gmail.com>
2025-09-01 21:28:49 +0530

    removed unused packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,41 +9,6 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals"),
-{
-  ignores:
-    [
-      ".next/**",
-      ".env",
-      "node_modules",
-      "public/**",
-      "next.config.js",
-      "postcss.config.js"
-    ]
-},
-{
-  languageOptions: { globals: { ...globals.browser, ...globals.node } }
-},
-{ files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-{ files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-{
-  rules: {
-    "no-unused-vars": ["warn"],
-    "no-undef": ["warn"],
-    "quotes": ["warn", "double", { "avoidEscape": true }],
-    "semi": ["warn", "always"],
-    "indent": ["warn", 2],
-    "class-methods-use-this": "warn",
-    "eol-last": ["warn", "always"],
-    "no-unused-expressions": ["warn"],
-    "no-multiple-empty-lines": ["error", { "max": 1 }],
-    "no-trailing-spaces": ["warn"],
-    "no-useless-constructor": 0,
-    "no-loop-func": 0,
-  }
-},
-
-js.configs.recommended,
-];
+const eslintConfig = [...compat.extends("next/core-web-vitals")];
 
 export default eslintConfig;
