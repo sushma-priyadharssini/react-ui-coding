d2cd295674d94324cf4cf083945890dc8f751211
Sushma Priyadharssini KV <kvsushmapriyadharssini@gmail.com>
2025-11-16 22:18:14 +0530

    scroll to top on navigation to next page

--- src/libs/home/components/project-list/index.js
+++ src/libs/home/components/project-list/index.js
@@ -1,18 +1,29 @@
 "use client";
-import { ROUTES } from "../../routes"
 import { Card } from "../project-card"
 import styles from "./project.module.css"
 import { ITEMS_PER_PAGE } from "../constants";
 import { useAppContext } from "@/libs/app-context";
+import { useEffect, useRef } from "react";
 
 
 export const ProjectList = ({ projects }) => {
     const { pagination: { currentPage } } = useAppContext();
+    const projectListRef = useRef(null);
+
+    useEffect(() => {
+        if (projectListRef.current) {
+            projectListRef.current.scrollTo({
+                top: 0,
+                behavior: "auto",
+            });
+        }
+    }, [currentPage]);
+
     const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
     const endIndex = Math.min(startIndex + ITEMS_PER_PAGE, projects.length);
     const filteredPages = projects.slice(startIndex, endIndex);
 
-    return <nav className={styles.projectList}>
+    return <nav ref={projectListRef} className={styles.projectList}>
         <ul className={styles.gridContainer}>
             {filteredPages.map((app) => {
                 const { name, description, path, github } = app;
